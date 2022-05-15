6aa090e8b5148aa1a41801a426e7efcdff5a2b85
Матвей Строков <strokov.matvei@yandex.ru>
2022-05-15 12:26:11 +0300

    after first review third  attempt

--- homework.py
+++ homework.py
@@ -130,8 +130,8 @@ def main():
             if homeworks:
                 for homework in homeworks:
                     parse_homework: str = parse_status(homework)
-                    if telegram_message != parse_homework:
-                        send_message(bot, parse_homework)
+                    # if telegram_message != parse_homework:
+                    send_message(bot, parse_homework)
             time.sleep(settings.RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
